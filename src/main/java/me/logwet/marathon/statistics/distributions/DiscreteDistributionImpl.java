7f9e7cb50714e41767c4039a1bc133b460753629
logwet <logwet@users.noreply.github.com>
2021-11-14 21:31:13 +1100

    Rename implementations of EnumeratedIntegerDistribution

--- src/main/java/me/logwet/marathon/statistics/distributions/DiscreteDistributionImpl.java
+++ src/main/java/me/logwet/marathon/statistics/distributions/DiscreteDistributionImpl.java
@@ -2,8 +2,8 @@ package me.logwet.marathon.statistics.distributions;
 
 import me.logwet.marathon.statistics.util.AbstractDiscreteDistribution;
 
-public class EnumeratedIntegerDistributionImpl extends AbstractDiscreteDistribution {
-    public EnumeratedIntegerDistributionImpl(int s, int n, double[] p) {
+public class DiscreteDistributionImpl extends AbstractDiscreteDistribution {
+    public DiscreteDistributionImpl(int s, int n, double[] p) {
         super(s, n, p);
     }
 }
--- src/main/java/me/logwet/marathon/statistics/distributions/ProductOfDiscreteDistributionsImpl.java
+++ src/main/java/me/logwet/marathon/statistics/distributions/ProductOfDiscreteDistributionsImpl.java
@@ -3,9 +3,8 @@ package me.logwet.marathon.statistics.distributions;
 import me.logwet.marathon.statistics.util.AbstractDiscreteDistribution;
 import me.logwet.marathon.statistics.util.AbstractProductOfDiscreteDistributions;
 
-public class ProductOfEnumeratedIntegerDistributionsImpl
-        extends AbstractProductOfDiscreteDistributions {
-    public ProductOfEnumeratedIntegerDistributionsImpl(
+public class ProductOfDiscreteDistributionsImpl extends AbstractProductOfDiscreteDistributions {
+    public ProductOfDiscreteDistributionsImpl(
             AbstractDiscreteDistribution d1, AbstractDiscreteDistribution d2) {
         super(d1, d2);
     }
